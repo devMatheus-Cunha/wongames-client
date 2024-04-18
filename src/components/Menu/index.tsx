6b7b2040119ec7e2652233bcb55972c189eb5e14
Matheus Cunha <devmatheusgr@gmail.com>
2024-04-18 11:41:06 +0100

    create Menu with tests and styles

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,35 +1,80 @@
+import { useState } from 'react'
 import {
-  Menu as MenuIcon,
+  Close as CloseIcon,
   Search as SearchIcon,
   ShoppingCart as ShoppingCartIcon,
-  Close as CloseIcon
+  Menu as MenuIcon
 } from '@styled-icons/material'
 
-import Logo from '../Logo'
-
+import Button from '../../components/Button'
+import Logo from '../../components/Logo'
+import MediaMatch from '../MediaMatch'
 import * as S from './styles'
-import { useState } from 'react'
 
-const Menu = () => {
+type MenuProps = {
+  userName?: string
+}
+
+const Menu = ({ userName }: MenuProps) => {
   const [isOpen, setIsOpen] = useState(false)
+
   return (
     <S.Wrapper>
-      <S.IconWrapper>
-        <MenuIcon aria-label="Open Menu" onClick={() => setIsOpen(true)} />
-      </S.IconWrapper>
+      <MediaMatch lessThan="medium">
+        <S.IconWrapper onClick={() => setIsOpen(true)}>
+          <MenuIcon aria-label="Open Menu" />
+        </S.IconWrapper>
+      </MediaMatch>
+
       <S.LogoWrapper>
         <Logo hideOnMobile />
       </S.LogoWrapper>
+
+      <MediaMatch greaterThan="medium">
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+        </S.MenuNav>
+      </MediaMatch>
+
       <S.MenuGroup>
         <S.IconWrapper>
-          <SearchIcon aria-label="Searchs" />
+          <SearchIcon aria-label="Search" />
         </S.IconWrapper>
+
         <S.IconWrapper>
           <ShoppingCartIcon aria-label="Open Shopping Cart" />
         </S.IconWrapper>
+
+        <MediaMatch greaterThan="medium">
+          {!userName && <Button>Sign in</Button>}
+        </MediaMatch>
       </S.MenuGroup>
+
       <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
         <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+          {!!userName && (
+            <>
+              <S.MenuLink href="#">My account</S.MenuLink>
+              <S.MenuLink href="#">Wishlist</S.MenuLink>
+            </>
+          )}
+        </S.MenuNav>
+
+        {!userName && (
+          <S.RegisterBox>
+            <Button fullWidth size="large">
+              Log in now
+            </Button>
+            <span>or</span>
+            <S.CreateAccount href="#" title="Sign Up">
+              Sign Up
+            </S.CreateAccount>
+          </S.RegisterBox>
+        )}
       </S.MenuFull>
     </S.Wrapper>
   )
--- src/components/Menu/stories.tsx
+++ src/components/Menu/stories.tsx
@@ -7,3 +7,10 @@ export default {
 } as Meta
 
 export const Default: StoryObj = {}
+
+Default.parameters = {
+  layout: 'fullscreen',
+  background: {
+    default: 'dark'
+  }
+}
