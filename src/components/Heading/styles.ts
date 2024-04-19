ce4bd66a7106fe20a45e076f6009d163031f9682
Matheus Cunha <devmatheusgr@gmail.com>
2024-04-19 11:46:56 +0100

    add new functionalities and tests in my component Heading

--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -1,14 +1,28 @@
 import styled, { css } from 'styled-components'
 import media from 'styled-media-query'
-import { HeadingProps } from '.'
+import { HeadingProps, LineColors } from '.'
 import { DefaultTheme } from 'styled-components/dist/types'
 
 const warapperModifiers = {
-  lineLeft: (theme: DefaultTheme) => css`
-    padding-left: ${theme.spacings.xxsmall};
-    border-left: 0.7rem solid ${theme.colors.secondary};
+  small: (theme: DefaultTheme) => css`
+    font-size: ${theme.font.sizes.medium};
+    &::after {
+      width: 3rem;
+    }
   `,
-  lineBottom: (theme: DefaultTheme) => css`
+  medium: (theme: DefaultTheme) => css`
+    font-size: ${theme.font.sizes.xlarge};
+
+    ${media.greaterThan('medium')`
+     font-size: ${theme.font.sizes.xxlarge};
+    `};
+  `,
+  lineLeft: (theme: DefaultTheme, lineColors?: LineColors) => css`
+    padding-left: ${theme.spacings.xxsmall};
+    border-left: 0.7rem solid ${theme.colors[lineColors!]};
+  `,
+
+  lineBottom: (theme: DefaultTheme, lineColors?: LineColors) => css`
     position: relative;
     margin-bottom: ${theme.spacings.medium};
 
@@ -16,23 +30,19 @@ const warapperModifiers = {
       content: '';
       position: absolute;
       left: 0;
-      bottom: -5rem;
+      bottom: -0.5rem;
       width: 5rem;
-      border-bottom: 0.5rem solid ${theme.colors.primary};
+      border-bottom: 0.5rem solid ${theme.colors[lineColors!]};
     }
   `
 }
 
 export const Wrapper = styled.h2<HeadingProps>`
-  ${({ theme, color, lineLeft, lineBottom }) => css`
-    font-size: ${theme.font.sizes.xlarge};
+  ${({ theme, color, lineLeft, lineBottom, size, lineColor }) => css`
     color: ${theme.colors[color!]};
 
-    ${media.greaterThan('medium')`
-     font-size: ${theme.font.sizes.xxlarge};
-    `};
-
-    ${lineLeft && warapperModifiers.lineLeft(theme)}
-    ${lineBottom && warapperModifiers.lineBottom(theme)}
+    ${lineLeft && warapperModifiers.lineLeft(theme, lineColor)}
+    ${lineBottom && warapperModifiers.lineBottom(theme, lineColor)}
+    ${!!size && warapperModifiers[size](theme)}
   `}
 `
